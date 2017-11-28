e0a7be7accf23b948038b62924141329404120ec
Lomboy <Pia.Lomboy@state.ca.gov>
2017-11-28 15:14:47 -0800

    use memory router, replace index.html, add sp-pnp-js

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,14 +10,13 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import pnp from 'sp-pnp-js';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <h1>
-        <FormattedMessage {...messages.header} />
+        <span>sdfdsf</span>
       </h1>
     );
   }
