08d19fb6a8bb921dccb4fd79ccbbc8d7ece4fa94
Lomboy <Pia.Lomboy@state.ca.gov>
2017-11-29 14:41:36 -0800

    add site urls to readme

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,6 +12,7 @@
 import React from 'react';
 import pnp from 'sp-pnp-js';
 window.pnp = pnp;
+
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
